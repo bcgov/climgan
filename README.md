4658460404da3e21224a1762ca797a1b446ea0e8
Kiri <kiridaust@gmail.com>
2025-06-23 12:43:53 -0700

    fix setup and readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # climgan
-Python package for training a GAN PRISM emulator, using the Wasserstein Generative Adversarial Network
+Python package for training a GAN PRISM emulator, using the stochasticv Wasserstein Generative Adversarial Network as described in https://journals.ametsoc.org/view/journals/aies/4/2/AIES-D-24-0044.1.xml
+
+# Installation
+A working version of CUDA with Pytorch is required for this repo. 
+
+1. Create a python virtual environment and activate it:
+
+    ```python3 -m venv myvenv```
+
+    ```source myvenv/bin/activate```
+
+2. Install requirements
+    ```pip install -r requirements.txt```
+
+3. Install climgan (-e is essential if you want to customize the code)
+```pip install -e /path/to/cloned/climgan/```
--- setup.py
+++ setup.py
@@ -3,31 +3,29 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="DoWnGAN",
-    description="DOwnscaling with WassersteiN Generative Adversarial Networks for Super Resolution",
+    name="climgan",
+    description="Prism Emulation with Wasserstein GANs",
     keywords="AI deep learning generative super resolution downscaling",
     packages=find_packages(),
-    version="0.1dev",
-    author="Nic Annau, Kiri Daust",
+    version="0.1",
+    author="Kiri Daust, Nic Annau",
     author_email="kiridaust@uvic.ca",
     zip_safe=True,
     scripts=[
-        "DoWnGAN/GAN/train.py",
-        "DoWnGAN/GAN/dataloader.py",
-        "DoWnGAN/GAN/losses.py",
-        "DoWnGAN/config/config.py",
-        "DoWnGAN/config/hyperparams.py",
-        "DoWnGAN/helpers/prep_gan.py",
-        "DoWnGAN/helpers/wrf_times.py",
-        "DoWnGAN/helpers/gen_experiment_datasets.py",
-        "DoWnGAN/networks/critic.py",
-        "DoWnGAN/networks/generator.py",
-        "DoWnGAN/mlflow_tools/gen_grid_plots.py",
+        "climgan/GAN/train.py",
+        "climgan/GAN/dataloader.py",
+        "climgan/GAN/losses.py",
+        "climgan/config/config.py",
+        "climgan/config/hyperparams.py",
+        "climgan/networks/critic_covariates.py",
+        "climgan/networks/prism_generator.py",
+        "climgan/mlflow_tools/gen_grid_plots.py",
+        "climgan/mlflow_tools/mlflow_epoch.py"
     ],
     install_requires=["numpy", "dask", "torch", "xarray", "scikit-learn"],
     extras_require = {"dask": "distributed"},
-    package_dir={"DoWnGAN": "DoWnGAN"},
-    package_data={"DoWnGAN": ["data/*", "DoWnGAN/"]},
+    package_dir={"climgan": "climgan"},
+    package_data={"climgan": ["data/*", "climgan/"]},
     classifiers="""
         Intended Audience :: Science/Research
         License :: GNU General Public License v3 (GPLv3)
